d63336c13d4e8b2c3e9386e50d8115d05356bece
Yassir-Chakour <yassir@shinobiautomation.com>
2026-04-21 17:50:50 +0100

    feat: implement automated Google Sheets worksheet creation based on initial state class list

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
+from unittest import result
+
 from nodes.database import fetch_students_node
 from nodes.processor import proccess_data_node
 from utils.google_client import google_client
+from nodes.sheets import sheets_managment
 
-# initial_state = {
-#     "students": [],
-#     "class_list": [],
-#     "logs": []
-# }
+initial_state = {
+    "students": [],
+    "class_list": ['shinobi', '9bi7', 'xwiya'],
+    "logs": []
+}
 #
 # result = fetch_students_node(initial_state)
 # result = proccess_data_node(result)
-result = google_client()
+# result = google_client()
+result = sheets_managment(initial_state)
 
 print("Final State:")
 print(result)
--- src/nodes/sheets.py
+++ src/nodes/sheets.py
@@ -1,1 +1,18 @@
-import gspread  
+import gspread
+from state import AgentState
+from utils.google_client import google_client
+
+def sheets_managment(state: AgentState):
+    sh = google_client()
+    classes = state['class_list']
+    existing_titles = [ws.title for ws in sh.worksheets()]
+
+    for cls in classes:
+        if cls not in existing_titles:
+            worksheet = sh.add_worksheet(title=cls, rows=100, cols=100)
+            worksheet.update_acell("A1", "Student Name")
+            worksheet.update_acell("B1", "Grades")
+            existing_titles.append(cls)
+            print((f"Worksheet {worksheet.title} added..."))
+        else:
+            print(f"Worksheet '{cls}' already exists. Skipping...")
